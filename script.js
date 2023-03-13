2b243ee8643e452f7f614d7547e467badfdc8435
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-03-13 21:07:14 +0100

    add base styles and HTML structure

---
+++ script.js
@@ -0,0 +1,41 @@
+const button = document.querySelector('.nav__button')
+
+const buttonpp = document.querySelector('.hamburger')
+const backBlack = document.querySelector('.back_black')
+const menu = document.querySelector('.menu_slide_left')
+const close6 = document.querySelector('.close')
+
+console.log(buttonpp, backBlack)
+
+if(button === true){
+    console.log('yes')
+}
+const open =  function () {
+    document.querySelector(".form").classList.add('open');
+document.querySelector(".form").classList.remove('open');
+
+document.querySelector(".form").classList.add('close');
+document.querySelector(".form").classList.remove('close');
+
+if ( document.querySelector(".form").classList.contains('open') )
+{
+document.querySelector(".form").classList.toggle('close');
+}
+}
+
+
+buttonpp.addEventListener('click', function(){
+    alert('click')
+    
+    menu.style.transform = "translateX(0%)"
+})
+
+close6.addEventListener('click', function(){
+    menu.style.transform = "translateX(-100%)"
+})
+
+
+
+
+
+
