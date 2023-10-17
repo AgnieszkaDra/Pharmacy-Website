c26f21ab4718e451ae9be8868a07d831413e1c9d
Agnieszka Draganczyk <Agnieszka.127@interia.pl>
2023-10-17 15:17:10 +0200

    some changes in header HTML

--- src/js/index.js
+++ src/js/index.js
@@ -1,41 +1,9 @@
-
-
-
-const button = document.querySelector('.nav__button')
-
-const buttonpp = document.querySelector('.hamburger')
-const backBlack = document.querySelector('.back_black')
+const hamburger = document.querySelector('.hamburger')
 const nav = document.querySelector('nav')
 const buttonClose = document.querySelector('.nav__button.close')
-const menu = document.querySelector('.menu_slide_left')
-const close6 = document.querySelector('.close')
-const header = document.querySelector('header')
-const body = document.querySelector('body')
 
-
-
-
-if(button === true){
-
-}
-const open =  function () {
-    document.querySelector(".form").classList.add('open');
-document.querySelector(".form").classList.remove('open');
-
-document.querySelector(".form").classList.add('close');
-document.querySelector(".form").classList.remove('close');
-
-if ( document.querySelector(".form").classList.contains('open') )
-{
-document.querySelector(".form").classList.toggle('close');
-}
-}
-
-
-buttonpp.addEventListener('click', function(){
-    alert('click')
+hamburger.addEventListener('click', function(){
     nav.classList.add('nav__block')
-   
 })
 
 buttonClose.addEventListener('click', function(){
@@ -43,9 +11,29 @@ buttonClose.addEventListener('click', function(){
     nav.classList.remove('nav__block')
 })
 
-close6.addEventListener('click', function(){
-    menu.style.transform = "translateX(-100%)"
-})
+
+// if(button === true){
+
+// }
+// const open =  function () {
+//     document.querySelector(".form").classList.add('open');
+// document.querySelector(".form").classList.remove('open');
+
+// document.querySelector(".form").classList.add('close');
+// document.querySelector(".form").classList.remove('close');
+
+// if ( document.querySelector(".form").classList.contains('open') )
+// {
+// document.querySelector(".form").classList.toggle('close');
+// }
+// }
+
+
+
+
+
+
+
 
 
 
